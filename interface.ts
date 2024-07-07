37e5d01f0846cbac69a5ef6b8dfe168c936a6a16
Hendryboyz <hendry.js1247@gmail.com>
2024-07-07 16:49:14 +0800

    feat: abstract receiver and sender

---
+++ interface.ts
@@ -0,0 +1,18 @@
+import { Message } from "./dto";
+
+  
+export interface ISender {
+  send(data: any): void;
+  handleAck(messageId: string): void;
+}
+
+export interface IReceiver {
+  receive(message: Message): void;
+  confirmAckReceived(messageId: string): void;
+}
+
+export interface IMessageFactory {
+  createSender(): ISender;
+  createReceiver(): IReceiver;
+}
+
